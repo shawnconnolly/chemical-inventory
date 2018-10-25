03b838be250f94836c0112a1903e8daef09f35f1
Shawn Connolly <shwnkcnnlly@gmail.com>
2018-10-25 18:08:27 -0500

    Added Synthetic Event workaround

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavigationItems from './Navigation/NavigationItems/NavigationItems'
 import Rooms from './Rooms/Rooms'
--- src/Rooms/Rooms.js
+++ src/Rooms/Rooms.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 
 class Rooms extends Component {
+    state = {
+        Rooms: [{ name: "103", location: "North Building" },
+        { name: "104", location: "North Building" }],
+        roomName: '',
+        roomLocation: ''
+    };
+
+
+
+    nameUpdated = (event) => {
+        event.persist();
+        console.log(event);
+        this.setState({roomName : event.target.value});
+        
+    }
+
     render() {
+        const listItems = this.state.Rooms.map((room) =>
+            <li key={room.name}>{room.name} - {room.location}</li>
+        );
+
         return (
             <div>
-                <label for="name">Name</label>
-                <input type="text" name="name" class="form-control"/>
-                <label for="location">Location</label>
-                <input type="text" name="location" class="form-control"/>
-                <button class="btn btn-primary">Add</button>
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" className="form-control" onChange={(e) => this.nameUpdated(e)} />
+                    <p>{this.state.roomName}</p>
+                    <label for="location">Location</label>
+                    <input type="text" name="location" className="form-control" />
+                    <button class="btn btn-primary" onClick={this.addRoom}>Add</button>
+                    <button class="btn btn-primary">Remove</button>
+                    <button class="btn btn-primary">Edit</button>
+                </div>
+                <ul>
+                    {listItems}
+                </ul>
             </div>
         );
     }
