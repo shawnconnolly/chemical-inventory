6943aef621f901ad6588ff8ae0e486c915c2a194
Shawn Connolly <shwnkcnnlly@gmail.com>
2018-10-30 23:22:09 -0500

    Fixed console errors, added more reactstrap buttons

--- src/Chemicals/Chemicals.js
+++ src/Chemicals/Chemicals.js
@@ -3,6 +3,7 @@ import classes from './Chemicals.module.css'
 import { connect } from 'react-redux';
 import * as actions from '../Store/Actions/index';
 import { Prompt } from 'react-router';
+import { Button } from 'reactstrap';
 
 class Chemicals extends Component {
 
@@ -21,50 +22,50 @@ class Chemicals extends Component {
                 <h1>{this.props.roomName}</h1>
                 <h2>{this.props.roomLocation}</h2>
                 <div className="classes.Chemicals">
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input type="text"
                         name="name"
                         className="form-control"
                         value={this.props.chemName}
                         onChange={(e) => this.props.onChemicalNameUpdated(e.target.value)} />
-                    <label for="tradename">Trade Name</label>
+                    <label htmlFor="tradename">Trade Name</label>
                     <input type="text"
                         name="tradename"
                         className="form-control"
                         value={this.props.chemTradeName}
                         onChange={(e) => this.props.onChemicalTradeNameUpdated(e.target.value)} />
-                    <label for="quantity">Quantity</label>
+                    <label htmlFor="quantity">Quantity</label>
                     <input type="text"
                         name="quantity"
                         className="form-control"
                         value={this.props.chemQty}
                         onChange={(e) => this.props.onChemicalQtyUpdated(e.target.value)} />
-                    <label for="uom">Unit of Measure</label>
+                    <label htmlFor="uom">Unit of Measure</label>
                     <input type="text"
                         name="uom"
                         className="form-control"
                         value={this.props.chemQtyUoM}
                         onChange={(e) => this.props.onChemicalQtyUoMUpdated(e.target.value)} />
-                    <label for="cabinet">Cabinet</label>
+                    <label htmlFor="cabinet">Cabinet</label>
                     <input type="text"
                         name="cabinet"
                         className="form-control"
                         value={this.props.cabinet}
                         onChange={(e) => this.props.onChemicalCabinetUpdated(e.target.value)} />
                     <hr></hr>
-                    <button class="btn btn-primary"
+                    <Button className="btn btn-primary"
                         onClick={() => this.props.onChemicalAdded({ name: this.props.chemName, tradeName: this.props.chemTradeName, quantity: this.props.chemQty, UoM: this.props.chemQtyUoM, cabinet: this.props.cabinet })}
-                        disabled={this.props.selectedChemical !== -1}>Add</button>
-                    <button class="btn btn-primary"
+                        disabled={this.props.selectedChemical !== -1}>Add</Button>
+                    <Button className="btn btn-primary"
                         disabled={this.props.selectedChemical === -1}
-                        onClick={() => this.props.onChemicalRemoved(this.props.selectedChemical)}>Remove</button>
-                    <button class="btn btn-primary"
+                        onClick={() => this.props.onChemicalRemoved(this.props.selectedChemical)}>Remove</Button>
+                    <Button className="btn btn-primary"
                         disabled={this.props.selectedChemical === -1}
-                        onClick={() => this.props.onEditChemical({ name: this.props.chemName, tradeName: this.props.chemTradeName, quantity: this.props.chemQty, UoM: this.props.chemQtyUoM, cabinet: this.props.cabinet }, this.props.selectedChemical)}>Edit</button>
-                    <button class="btn btn-primary"
-                        onClick={() => this.props.onSave(this.props.rooms, this.props.token)}>Save Inventory</button>
-                    <button class="btn btn-primary"
-                        onClick={() => this.props.onLoad(this.props.token)}>Load Inventory</button>
+                        onClick={() => this.props.onEditChemical({ name: this.props.chemName, tradeName: this.props.chemTradeName, quantity: this.props.chemQty, UoM: this.props.chemQtyUoM, cabinet: this.props.cabinet }, this.props.selectedChemical)}>Edit</Button>
+                    <Button className="btn btn-primary"
+                        onClick={() => this.props.onSave(this.props.rooms, this.props.token)}>Save Inventory</Button>
+                    <Button className="btn btn-primary"
+                        onClick={() => this.props.onLoad(this.props.token)}>Load Inventory</Button>
                 </div>
                 <ul>
                     {listItems}
